b23af160cb3e882cef31d5f19d833a55ee73b97d
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-10-23 20:46:35 -0400

    //Añadir información y modificar página principal

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Next.js Intro</title>
+      </Head>
+
+      <main>
+        <h1>Jhonatan, Robert and Stiven</h1>
+        <p>2023</p>
+      </main>
+    </div>
+  )
+}
