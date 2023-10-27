c088c67bb475bba5490c697a330ad69e737be910
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-10-27 10:00:01 -0400

    //Lists

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css';
 import type { NextPage } from 'next'
 import Head from "next/head"
+import Link from 'next/link';
 const Home: NextPage = () => {
 
     return (
@@ -24,12 +25,102 @@ const Home: NextPage = () => {
             </div>
 
             <div className='bg-black max-w-3xl mt-20 h-screen p-12 mx-auto rounded-lg text-white'>
-                <h2>Mejor Framework</h2>
+                <h2>Mejor biblioteca UI</h2>
                 <ul>
                     <li>React</li>
-                    <li>Vue</li>
+                    <li>Vue.js</li>
                     <li>Angular</li>
+                    <li>Svelte</li>
+                    <li>Lit</li>
+                    <li>Ember</li>
+                    <li>Alpine.js</li>
+                    <li>Preact</li>
+                    <li>Stimulus</li>
+                    <li>Solid</li>
+                    
                 </ul>
+
+                <h2>Mejor framework Backend</h2>
+                <ul>
+                    <li>Nest.js</li>
+                    <li>Express</li>
+                    <li>Fastify</li>
+                    <li>Hapi</li>
+                </ul>
+
+                <h2>Mejor framework FullStack</h2>
+                <ul>
+                    <li>Next.js</li>
+                    <li>Nuxt</li>
+                    <li>SvelteKit</li>
+                    <li>Remix</li>
+                    <li>BlitzJS</li>
+                    
+                </ul>
+
+                <h2>Mejores empaquetadores de código</h2>
+                <ul>
+                    <li>Webpack</li>
+                    <li>Rollup</li>
+                    <li>Parcel</li>
+                    <li>Esbuild</li>
+                    <li>Vite</li>
+                    
+                </ul>
+
+                <h2>Mejor librería CSS en JS</h2>
+                <ul>
+                    <li>Stitches</li>
+                    <li>Styled Components</li>
+                    <li>Emotion</li>
+                    
+                </ul>
+
+                <h2>Mejor framework CSS</h2>
+                <ul>
+                    <li>Tailwind</li>
+                    
+                
+                </ul>
+
+                <h2>Mejor herramienta de testing</h2>
+                <ul>
+                    <li>Jest</li>
+                    <li>Testing Library</li>
+                    <li>Vitest</li>
+                    
+                
+                </ul>
+
+                <h2>Mejor test E2E</h2>
+                <ul>
+                    <li>Cypress</li>
+                    <li>Playwright</li>
+                    <li>Pupeeteer</li>
+                    
+                
+                </ul>
+
+                <h2>Mejor BaaS (Backend as a service)</h2>
+                <ul>
+                    <li>Firebase</li>
+                    <li>Supabase</li>
+                    <li>Airtable</li>
+                    <li>Amplify</li>
+                    <li>Mongo Stitch</li>
+                
+                </ul>
+
+                <h2>Mejor catalogo de componentes</h2>
+                <ul>
+                    <li>Chakra UI</li>
+                    <li>Material UI</li>
+                    <li>NextUI</li>
+                    <li>Antd</li>
+                    <li>DaisyUI</li>
+                    <li>React Semantic UI</li>
+                </ul>
+
             </div>
         </section>
     </>
