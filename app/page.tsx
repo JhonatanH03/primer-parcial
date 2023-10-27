f10907ca3a5005afe466193ee781f02b8056e159
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-10-27 09:01:35 -0400

    //Modification of page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import type { NextPage } from 'next'
 import Head from "next/head"
-
 const Home: NextPage = () => {
 
     return (
@@ -9,28 +8,32 @@ const Home: NextPage = () => {
         <Head>
             <title> vota. dev - Vota los mejores del año en el mundo del desarrollo web</title>
         </Head>
-        <header/>
+        <header className='flex items-center w-full justify-between max-w-4xl mx-auto'>
+            <h1 className='font-bold text-xl'> 🗳️ vota.dev</h1>
+            <div>
+            </div>
+        </header>
         <section className='mt-48'>
-        <h1 className='text-black text-9xl text-center font-extrabold'>
-            Vota por la mejor tecnología del desarrollo web.
-        </h1>
-        <div className='flex-col items-center mt-10 max-w-lg text-center'>
-          <p className='text-black opacity-90 text-2xl'>
-            Decide qué stack, tecnologías y frameworks han sido los mejores del año. ¡Cada voto cuenta!
-          </p> 
-        </div>
+            <h1 className='text-black text-9xl text-center font-extrabold'>
+                Vota por la mejor tecnología del desarrollo web.
+            </h1>
+            <div className='flex flex-col items-center mt-8 max-w-lg text-center'>
+              <p className='text-black opacity-90 text-2xl'>
+                Decide qué stack, tecnologías y frameworks han sido los mejores del año. ¡Cada voto cuenta!
+              </p>
+            </div>
 
-        <div className='bg-black max-w-3xl mt-20 h-screen p-12 mx-auto rounded-lg text-white'>
-          <h2>Mejor Framework</h2>
-          <ul>
-            <li>React</li>
-            <li>Vue</li>
-            <li>Angular</li>
-          </ul>
-        </div>
-
+            <div className='bg-black max-w-3xl mt-20 h-screen p-12 mx-auto rounded-lg text-white'>
+                <h2>Mejor Framework</h2>
+                <ul>
+                    <li>React</li>
+                    <li>Vue</li>
+                    <li>Angular</li>
+                </ul>
+            </div>
         </section>
     </>
     )
 }
+
 export default Home
