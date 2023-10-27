47dbebf31a8c402e336e51bb89a2e12960cc5b47
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-10-27 15:16:19 -0400

    //Add the boxes where the logos will go

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,19 @@ import 'tailwindcss/tailwind.css';
 import type { NextPage } from 'next'
 import Head from "next/head"
 import Link from 'next/link';
-const Home: NextPage = () => {
+import { Header } from '@/components/Header';
 
 const SectionTitle = ({children}:{children: string}) => <h2 className='text-xl font-bold '>{children}</h2>
+
+const Box = ({ title }: { title: string }) => <div>
+    <div className='rounded flex flex-col items-center justify-center border border-gray-100 py-10 shadow-lg'>
+      <div className='p-4'>{title}</div>
+    </div>
+</div>
+
+const Home: NextPage = () => {
+    
+
 
     return (
     <>
@@ -26,101 +36,90 @@ const SectionTitle = ({children}:{children: string}) => <h2 className='text-xl f
               </p>
             </div>
 
-            <div className='sticky top-0 bg-black max-w-3xl mt-20 p-12 mx-auto rounded-lg text-white'>
+            <div className='sticky top-0 bg-white max-w-3xl mt-20 p-12 mx-auto rounded-lg text-black'>
                 <SectionTitle>Mejor biblioteca UI</SectionTitle>
                 <ul>
-                    <li>React</li>
-                    <li>Vue.js</li>
-                    <li>Angular</li>
-                    <li>Svelte</li>
-                    <li>Lit</li>
-                    <li>Ember</li>
-                    <li>Alpine.js</li>
-                    <li>Preact</li>
-                    <li>Stimulus</li>
-                    <li>Solid</li>
-                    
+                    <Box title="React" />
+                    <Box title="Vue.js" />
+                    <Box title="Angular" />
+                    <Box title="Svelte" />
+                    <Box title="Lit" />
+                    <Box title="Ember" />
+                    <Box title="Alpine.js" />
+                    <Box title="Preact" />
+                    <Box title="Stimulus" />
+                    <Box title="Solid" />
                 </ul>
 
                 <SectionTitle>Mejor framework Backend</SectionTitle>
                 <ul>
-                    <li>Nest.js</li>
-                    <li>Express</li>
-                    <li>Fastify</li>
-                    <li>Hapi</li>
+                    <Box title="Nest.js" />
+                    <Box title="Express" />
+                    <Box title="Fastify" />
+                    <Box title="Hapi" />
                 </ul>
 
                 <SectionTitle>Mejor framework FullStack</SectionTitle>
                 <ul>
-                    <li>Next.js</li>
-                    <li>Nuxt</li>
-                    <li>SvelteKit</li>
-                    <li>Remix</li>
-                    <li>BlitzJS</li>
-                    
+                <Box title="Next.js" />
+                    <Box title="Nuxt" />
+                    <Box title="SvelteKit" />
+                    <Box title="Remix" />
+                    <Box title="BlitzJS" />
                 </ul>
 
                 <SectionTitle>Mejores empaquetadores de código</SectionTitle>
                 <ul>
-                    <li>Webpack</li>
-                    <li>Rollup</li>
-                    <li>Parcel</li>
-                    <li>Esbuild</li>
-                    <li>Vite</li>
-                    
+                    <Box title="Webpack" />
+                    <Box title="Rollup" />
+                    <Box title="Parcel" />
+                    <Box title="Esbuild" />
+                    <Box title="Vite" />
                 </ul>
 
                 <SectionTitle>Mejor librería CSS en JS</SectionTitle>
                 <ul>
-                    <li>Stitches</li>
-                    <li>Styled Components</li>
-                    <li>Emotion</li>
-                    
+                    <Box title="Stitches" />
+                    <Box title="Styled Components" />
+                    <Box title="Emotion" />
                 </ul>
 
                 <SectionTitle>Mejor framework CSS</SectionTitle>
                 <ul>
-                    <li>Tailwind</li>
-                    
-                
+                    <Box title="Tailwind" />
                 </ul>
 
                 <SectionTitle>Mejor herramienta de testing</SectionTitle>
                 <ul>
-                    <li>Jest</li>
-                    <li>Testing Library</li>
-                    <li>Vitest</li>
-                    
-                
+                    <Box title="Jest" />
+                    <Box title="Testing Library" />
+                    <Box title="Vitest" />
                 </ul>
 
                 <SectionTitle>Mejor test E2E</SectionTitle>
                 <ul>
-                    <li>Cypress</li>
-                    <li>Playwright</li>
-                    <li>Pupeeteer</li>
-                    
-                
+                    <Box title="Cypress" />
+                    <Box title="Playwright" />
+                    <Box title="Puppeteer" />
                 </ul>
 
                 <SectionTitle>Mejor BaaS (Backend as a service)</SectionTitle>
                 <ul>
-                    <li>Firebase</li>
-                    <li>Supabase</li>
-                    <li>Airtable</li>
-                    <li>Amplify</li>
-                    <li>Mongo Stitch</li>
-                
+                    <Box title="Firebase" />
+                    <Box title="Supabase" />
+                    <Box title="Airtable" />
+                    <Box title="Amplify" />
+                    <Box title="Mongo Stitch" />
                 </ul>
 
-                <SectionTitle>Mejor catalogo de componentes</SectionTitle>
+                <SectionTitle>Mejor catálogo de componentes</SectionTitle>
                 <ul>
-                    <li>Chakra UI</li>
-                    <li>Material UI</li>
-                    <li>NextUI</li>
-                    <li>Antd</li>
-                    <li>DaisyUI</li>
-                    <li>React Semantic UI</li>
+                    <Box title="Chakra UI" />
+                    <Box title="Material UI" />
+                    <Box title="NextUI" />
+                    <Box title="Antd" />
+                    <Box title="DaisyUI" />
+                    <Box title="React Semantic UI" />
                 </ul>
 
             </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
-import {SessionProvider} from 'next-auth/react'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import '../app/globals.css'
 
-
-function MyApp ({Component : App, pageProps: {session, ...pageProps}}: AppProps) {
-    return(
-        <SessionProvider session={session}>
-            <div className= 'flex flex-col bg-yellow-400 min-h-screen aling-center'>
-                <App {...pageProps}/>
-            </div>
-        </SessionProvider>
-    )
+function MyApp({ Component: App, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <div className='flex flex-col bg-red-200 min-h-screen aling-center'>
+        <App {...pageProps} />
+      </div>
+    </SessionProvider>
+  )
 }
