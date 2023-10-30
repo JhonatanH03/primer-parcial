d8cee0f33de5cae941883278436354277e14c0d9
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-10-30 00:44:28 -0400

    //Finish the tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,28 @@ import type { NextPage } from 'next'
 import Head from "next/head"
 import Link from 'next/link';
 import { Header } from '@/components/organisms/Header';
+import React from 'react';
+
 
 interface SectionTitleProps {
     children: string
 }
 
 const SectionTitle = ({children}:SectionTitleProps) => 
-    <h2 className='text-xl font-bold '>{children}</h2>
+    <h2 className='text-4xl mb-6 font-bold '>{children}</h2>
 
 interface BoxProps {
     image: string
     title: string
 }
 
+
+
 const Box = ({image, title }: BoxProps) => <div>
-    <div className='rounded flex flex-col items-center justify-center border border-gray-100 py-10 shadow-lg'>
+    <div className='rounded flex flex-col items-center justify-center py-10 border-2 border-gray-200  '>
+        <div className='w-32 h-32 p-4'>
         <img alt={title} src={image} />
+        </div>
         <div className='font-bold mt-4 text-bold'>
             {title}
       </div>
@@ -39,7 +45,7 @@ const Home: NextPage = () => {
             <div>
             </div>
         </header>
-        <section className='mt-48'>
+        <section className='mt-40'>
             <h1 className='text-black text-9xl text-center font-extrabold'>
                 Vota por la mejor tecnología del desarrollo web.
             </h1>
@@ -49,9 +55,9 @@ const Home: NextPage = () => {
               </p>
             </div>
 
-            <div className='sticky bg-white max-w-3xl mt-20 p-12 mx-auto rounded-lg text-black shadow-yellow-950'>
+            <div className='sticky bg-white max-w-3xl mt-20 p-12 mx-auto rounded-lg text-black border-2 border-black'>
                 <SectionTitle>Mejor biblioteca UI</SectionTitle>
-                <ul>
+                <ul className='grid grid-cols-4 gap-4 mb-8'>
                     <Box image="/icons/react.svg" title="React" />
                     <Box image="/icons/vue.svg" title="Vue.js" />
                     <Box image="/icons/angular.svg"title="Angular" />
@@ -63,7 +69,7 @@ const Home: NextPage = () => {
                 </ul>
 
                 <SectionTitle>Mejor framework Backend</SectionTitle>
-                <ul>
+                <ul className='grid grid-cols-4 gap-4 mb-8'>
                     <Box image="/icons/nestjs.svg" title="Nest.js" />
                     <Box image="/icons/express.svg" title="Express" />
                     <Box image="/icons/fastify.svg" title="Fastify" />
@@ -71,16 +77,15 @@ const Home: NextPage = () => {
                 </ul>
 
                 <SectionTitle>Mejor framework FullStack</SectionTitle>
-                <ul>
+                <ul className='grid grid-cols-4 gap-4 mb-8'>
                     <Box image="/icons/next.svg" title="Next.js" />
                     <Box image="/icons/nuxt.svg" title="Nuxt" />
                     <Box image="/icons/sveltekit.svg" title="SvelteKit" />
                     <Box image="/icons/remix.svg" title="Remix" />
-                    <Box image="/icons/blitzjs.svg" title="BlitzJS" />
                 </ul>
 
                 <SectionTitle>Mejores empaquetadores de código</SectionTitle>
-                <ul>
+                <ul className='grid grid-cols-4 gap-4 mb-8'>
                     <Box image="/icons/webpack.svg" title="Webpack" />
                     <Box image="/icons/rollupjs.svg" title="Rollup" />
                     <Box image="/icons/parcel.svg" title="Parcel" />
@@ -88,27 +93,21 @@ const Home: NextPage = () => {
                     <Box image="/icons/vitejs.svg" title="Vite" />
                 </ul>
 
-                <SectionTitle>Mejor framework CSS</SectionTitle>
-                <ul>
-                    <Box image="/icons/tailwindcss.svg" title="Tailwind" />
-                </ul>
-
                 <SectionTitle>Mejor herramienta de testing</SectionTitle>
-                <ul>
+                <ul className='grid grid-cols-4 gap-4 mb-8'>
                     <Box image="/icons/jest.svg" title="Jest" />
-                    <Box image="/icons/testing-library.svg" title="Testing Library" />
                     <Box image="/icons/vitest.svg" title="Vitest" />
                 </ul>
 
                 <SectionTitle>Mejor test E2E</SectionTitle>
-                <ul>
+                                <ul className='grid grid-cols-4 gap-4 mb-8'>
                     <Box image="/icons/cypress.svg" title="Cypress" />
                     <Box image="/icons/playwright.svg" title="Playwright" />
-                    <Box image="/icons/puppeteer.svg" title="Puppeteer" />
+
                 </ul>
 
                 <SectionTitle>Mejor BaaS (Backend as a service)</SectionTitle>
-                <ul>
+                <ul className='grid grid-cols-4 gap-4 mb-8'>
                     <Box image="/icons/firebase.svg" title="Firebase" />
                     <Box image="/icons/supabase.svg" title="Supabase" />
                     <Box image="/icons/airtable.svg" title="Airtable" />
@@ -116,7 +115,7 @@ const Home: NextPage = () => {
                 </ul>
 
                 <SectionTitle>Mejor catálogo de componentes</SectionTitle>
-                <ul>
+                <ul className='grid grid-cols-4 gap-4 mb-8'>
                     <Box image="/icons/material-ui.svg" title="Material UI" />
                     <Box image="/icons/daisyUI.svg" title="DaisyUI" />
                     <Box image="/icons/semantic-ui.svg" title="React Semantic UI" />
